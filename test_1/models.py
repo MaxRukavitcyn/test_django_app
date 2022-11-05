308631b7cc1f2d01c667d9de3156dc261891cb36
Max <rukav77@gmail.com>
2022-11-06 01:42:14 +0500

    test migrations

--- test_1/models.py
+++ test_1/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-class Car:
-    def __init__(self):
-        self.id = 1
-        self.name = 'BMW'
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_pub = models.DateField(auto_now_add=True)
+    date_mod = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=True)
